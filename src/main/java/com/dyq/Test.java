c6947b8132d3456b8844cac6810960aae34146f3
dyq <992504740@qq.com>
2019-10-10 20:47:15 +0800

    hebingchongtu

--- src/main/java/com/dyq/Test.java
+++ src/main/java/com/dyq/Test.java
@@ -10,7 +10,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("ces");
         System.out.println("hehe ");
-System.out.println("ces");
-System.out.println("ces");
+        System.out.println("ces");
+        System.out.println("ces");
     }
 }
